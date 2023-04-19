6413b6f6586a3c210c85f2cee983238091211803
hiyee-gj <gyoengjae.h.dev@gmail.com>
2023-04-19 14:47:13 +0900

    :sparkles: BufferedReaderEx

---
+++ src/main/java/com/likelion/project1/week1/BufferedReaderExTest.java
@@ -0,0 +1,10 @@
+package com.likelion.project1.week1;
+
+import java.io.IOException;
+
+public class BufferedReaderExTest {
+    public static void main(String[] args) throws IOException {
+        BufferedReaderEx bufferedReaderEx = new BufferedReaderEx();
+        bufferedReaderEx.readALine();
+    }
+}
