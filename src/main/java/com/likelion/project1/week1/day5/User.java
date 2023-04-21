d2b29dd0d64f4c26e8cb0daf2dd48652e7625d84
hiyee-gj <gyoengjae.h.dev@gmail.com>
2023-04-21 15:15:17 +0900

    :sparkles: Add User and UserTest

---
+++ src/main/java/com/likelion/project1/week1/day5/User.java
@@ -0,0 +1,7 @@
+package com.likelion.project1.week1.day5;
+
+public class User {
+    String name;
+    String phoneNumber;
+    int age;
+}
---
+++ src/main/java/com/likelion/project1/week1/day5/UserTest.java
@@ -0,0 +1,15 @@
+package com.likelion.project1.week1.day5;
+
+public class UserTest {
+    public static void main(String[] args) {
+        User kyeongrok = new User();
+        kyeongrok.name = "김경록";
+        kyeongrok.phoneNumber = "000-0000-0000";
+        kyeongrok.age = 37;
+
+        User jaesung = new User();
+        jaesung.name = "재성 매니저님";
+        jaesung.phoneNumber = "000-0000-0000";
+        jaesung.age = 24;
+    }
+}
