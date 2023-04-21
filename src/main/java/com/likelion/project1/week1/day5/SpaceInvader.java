21656ca6cddd4059e7cc3e59305b35c96dcb1550
hiyee-gj <gyoengjae.h.dev@gmail.com>
2023-04-21 13:21:55 +0900

    :sparkles: Add SpaceInvaderMemberVariable
    git p

--- src/main/java/com/likelion/project1/week1/day5/SpaceInvader.java
+++ src/main/java/com/likelion/project1/week1/day5/SpaceInvader.java
@@ -4,10 +4,18 @@ public class SpaceInvader {
     private void moveLeft() {
         int location = 0;
         location = -1;
+        System.out.println("moveLeft = " + location);
     }
 
     private void moveRight() {
         int location = 0;
         location = 1;
+        System.out.println("moveRight = " + location);
+    }
+
+    public static void main(String[] args) {
+        SpaceInvader si = new SpaceInvader();
+        si.moveLeft();
+        si.moveRight();
     }
 }
---
+++ src/main/java/com/likelion/project1/week1/day5/SpaceInvaderMemberVariable.java
@@ -0,0 +1,24 @@
+package com.likelion.project1.week1.day5;
+
+public class SpaceInvaderMemberVariable {
+    int location; // 멤버변수
+
+    public void moveLeft() {
+        location = location - 1;
+        System.out.println("moveLeft = " + location);
+    }
+
+    public void moveRight() {
+        location = location + 1;
+        System.out.println("moveRight = " + location);
+    }
+
+    public static void main(String[] args) {
+        SpaceInvaderMemberVariable simv = new SpaceInvaderMemberVariable();
+        simv.moveLeft();
+        simv.moveRight();
+        simv.moveRight();
+        simv.moveRight();
+        simv.moveRight();
+    }
+}
