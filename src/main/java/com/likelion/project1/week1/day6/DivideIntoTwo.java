023d12f4cff80010a19ba848ba3b3b91b6566c24
hiyee-gj <gyoengjae.h.dev@gmail.com>
2023-04-24 10:14:40 +0900

    :sparkles: Add DiviedIntoTwo

---
+++ src/main/java/com/likelion/project1/week1/day6/DivideIntoTwo.java
@@ -0,0 +1,14 @@
+package com.likelion.project1.week1.day6;
+
+public class DivideIntoTwo {
+    public static void main(String[] args) {
+        float fOne = 1;
+        float fTwo = 2;
+
+        System.out.printf("%f \n", fOne + fOne / fTwo);
+
+        int iOne = 1;
+        int iTwo = 2;
+        System.out.printf("%d \n", iOne + iOne / iTwo);
+    }
+}
