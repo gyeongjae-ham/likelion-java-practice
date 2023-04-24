9e3ab4f8c2fa81cea80f635c25b686ba8af744c8
hiyee-gj <gyoengjae.h.dev@gmail.com>
2023-04-24 11:28:31 +0900

    :sparkles: Add FloatDigit

---
+++ src/main/java/com/likelion/project1/week1/day6/FloatAndDoubles.java
@@ -0,0 +1,14 @@
+package com.likelion.project1.week1.day6;
+
+public class FloatAndDoubles {
+    public static void main(String[] args) {
+        float f1 = 3.14f;
+        float f2 = 1.23e10f;
+
+        double d1 = 3.1415926535897;
+        double d2 = 1.23e100;
+
+        float result1 = f1 * 2.0f;
+        
+    }
+}
---
+++ src/main/java/com/likelion/project1/week1/day6/FloatDigit.java
@@ -0,0 +1,18 @@
+package com.likelion.project1.week1.day6;
+
+public class FloatDigit {
+    public static void main(String[] args) {
+        float f5 = 1.23e5f;
+        float f6 = 1.23e6f;
+        float f7 = 1.23e7f;
+        float f8 = 1.23e8f;
+
+        System.out.printf("f5: %f\n", f5);
+        System.out.printf("f6: %f\n", f6);
+        System.out.printf("f7: %f\n", f7);
+        System.out.printf("f8: %f\n", f8);
+
+        float f1 = 3.141592653589793f;
+        System.out.printf("%.15f", f1);
+    }
+}
