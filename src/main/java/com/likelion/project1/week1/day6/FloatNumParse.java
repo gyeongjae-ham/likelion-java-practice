e2549e7afa3998e70eb8f4005dfc67e09bec2679
hiyee-gj <gyoengjae.h.dev@gmail.com>
2023-04-24 13:32:24 +0900

    :sparkles: Add FloatNumParse

---
+++ src/main/java/com/likelion/project1/week1/day6/FloatNumParse.java
@@ -0,0 +1,11 @@
+package com.likelion.project1.week1.day6;
+
+public class FloatNumParse {
+    public static void main(String[] args) {
+        String s1 = "1.5";
+        String s2 = "2.5";
+
+        float result = Float.parseFloat(s1) + Float.parseFloat(s2);
+        System.out.println(result);
+    }
+}
