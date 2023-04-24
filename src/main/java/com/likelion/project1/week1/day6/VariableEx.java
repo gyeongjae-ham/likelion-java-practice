840af6a3e9f0926ee604b3ce705ccb4294e460cd
hiyee-gj <gyoengjae.h.dev@gmail.com>
2023-04-24 10:05:38 +0900

    :sparkles: Add VariableEx

---
+++ src/main/java/com/likelion/project1/week1/day6/VariableEx.java
@@ -0,0 +1,9 @@
+package com.likelion.project1.week1.day6;
+
+public class VariableEx {
+    public static void main(String[] args) {
+        String sOne = new String("1");
+        int iOne = 1;
+        float fOne = 1.0f;
+    }
+}
