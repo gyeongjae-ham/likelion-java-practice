2f3a77ddc60c204d273afc5089a63c80a566994d
hiyee-gj <gyoengjae.h.dev@gmail.com>
2023-04-25 14:28:43 +0900

    :sparkles: Add ArrayDefineAndPrint

---
+++ src/main/java/com/likelion/project1/week2/day7/ArrayDefineAndPrint.java
@@ -0,0 +1,12 @@
+package com.likelion.project1.week2.day7;
+
+import java.util.Arrays;
+
+public class ArrayDefineAndPrint {
+    public static void main(String[] args) {
+        int[] iArr = new int[]{1, 2, 3, 8};
+
+        System.out.println(iArr.length);
+        System.out.println(Arrays.toString(iArr));
+    }
+}
