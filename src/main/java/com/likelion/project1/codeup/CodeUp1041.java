a91a9568f065ff6c8cb9054742a201958827c116
hiyee-gj <gyoengjae.h.dev@gmail.com>
2023-04-25 15:33:52 +0900

    :sparkles: Add CodeUp ~41

--- src/main/java/com/likelion/project1/codeup/CodeUp1041.java
+++ src/main/java/com/likelion/project1/codeup/CodeUp1041.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 
 public class CodeUp1041 {
     public static void main(String[] args) throws IOException {
+        // 커밋
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int num = br.readLine().charAt(0) + 1;
         System.out.println((char) num);
