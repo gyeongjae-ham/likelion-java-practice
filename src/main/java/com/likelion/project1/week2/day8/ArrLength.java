cb073e9997e2af73a945bbd13be191464843ff89
hiyee-gj <gyoengjae.h.dev@gmail.com>
2023-04-26 14:23:00 +0900

    :sparkles: Add ArrLength

---
+++ src/main/java/com/likelion/project1/week2/day8/ArrLength.java
@@ -0,0 +1,8 @@
+package com.likelion.project1.week2.day8;
+
+public class ArrLength {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        System.out.println(arr.length);
+    }
+}
