a1bfdbf07bb4ae252fb24b95b931c9df0e25d4a6
hiyee-gj <gyoengjae.h.dev@gmail.com>
2023-04-26 10:02:43 +0900

    :sparkles: Add ClassInSchool

---
+++ src/main/java/com/likelion/project1/week2/day8/ClassInSchool.java
@@ -0,0 +1,7 @@
+package com.likelion.project1.week2.day8;
+
+public class ClassInSchool {
+    int no;
+    Teacher teacher;
+    Student[] students;
+}
---
+++ src/main/java/com/likelion/project1/week2/day8/Teacher.java
@@ -0,0 +1,7 @@
+package com.likelion.project1.week2.day8;
+
+public class Teacher {
+    String name;
+    int age;
+    String address;
+}
