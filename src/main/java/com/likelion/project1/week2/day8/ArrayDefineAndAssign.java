0ff8c3ddfd7e82155bc23836ee43d9907b0aee5f
hiyee-gj <gyoengjae.h.dev@gmail.com>
2023-04-26 11:17:38 +0900

    :sparkles: Add TwoDimArray

---
+++ src/main/java/com/likelion/project1/week2/day8/ArrayDefineAndAssign.java
@@ -0,0 +1,17 @@
+package com.likelion.project1.week2.day8;
+
+import java.util.Arrays;
+
+public class ArrayDefineAndAssign {
+    public static void main(String[] args) {
+        int[][] arr2 = {
+                {10, 20, 30},
+                {40, 50, 60},
+                {70, 80, 90}
+        };
+
+        System.out.println(Arrays.toString(arr2[0]));
+        System.out.println(Arrays.toString(arr2[1]));
+        System.out.println(Arrays.toString(arr2[2]));
+    }
+}
