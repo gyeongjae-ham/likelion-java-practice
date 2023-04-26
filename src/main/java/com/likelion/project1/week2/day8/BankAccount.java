cf86df79d11da8cf13537aba009786d7f3b4bdc5
hiyee-gj <gyoengjae.h.dev@gmail.com>
2023-04-26 15:54:34 +0900

    :sparkles: Add BankAccount and LikeAccumulate

---
+++ src/main/java/com/likelion/project1/week2/day8/BankAccount.java
@@ -0,0 +1,12 @@
+package com.likelion.project1.week2.day8;
+
+public class BankAccount {
+    public static void main(String[] args) {
+        int myAccount = 1_000_000;
+        myAccount = myAccount - 500_000;
+        System.out.println(myAccount);
+
+        myAccount = myAccount + 4_000_000;
+        System.out.println(myAccount);
+    }
+}
---
+++ src/main/java/com/likelion/project1/week2/day8/LikeAccumulate.java
@@ -0,0 +1,15 @@
+package com.likelion.project1.week2.day8;
+
+public class LikeAccumulate {
+    public static void main(String[] args) {
+        int likeCount = 0;
+        likeCount = likeCount + 1;
+        System.out.println(likeCount);
+
+        likeCount = likeCount + 1;
+        System.out.println(likeCount);
+
+        likeCount = likeCount - 1;
+        System.out.println(likeCount);
+    }
+}
