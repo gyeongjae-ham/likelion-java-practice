9c236c26dcdde8ec1a8b4b964f195f92728262c6
hiyee-gj <gyoengjae.h.dev@gmail.com>
2023-04-27 09:26:10 +0900

    :sparkles: Add ArraySum

---
+++ src/main/java/com/likelion/project1/week2/day9/ArraySum.java
@@ -0,0 +1,15 @@
+package com.likelion.project1.week2.day9;
+
+public class ArraySum {
+    public static void main(String[] args) {
+        int answer = 0;
+        int[] arr = {2, 1, 7, 9};
+
+        answer = answer + arr[0];
+        answer = answer + arr[1];
+        answer = answer + arr[2];
+        answer = answer + arr[3];
+
+        System.out.printf("answer: %d", answer);
+    }
+}
