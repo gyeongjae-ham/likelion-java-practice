aa9e2ce8600089d583b0d7d44507741d9c9c2143
hiyee-gj <gyoengjae.h.dev@gmail.com>
2023-04-27 10:15:19 +0900

    :sparkles: Add IncreaseFirst

---
+++ src/main/java/com/likelion/project1/week2/day9/IncreaseFirst.java
@@ -0,0 +1,15 @@
+package com.likelion.project1.week2.day9;
+
+public class IncreaseFirst {
+    public static void main(String[] args) {
+        int idx = 0;
+        int[] arr = {2, 1, 7, 4};
+        System.out.println(arr[idx]);
+
+        idx += 1;
+        System.out.println(arr[idx]);
+
+        System.out.println(arr[idx++]);
+        System.out.println(arr[++idx]);
+    }
+}
