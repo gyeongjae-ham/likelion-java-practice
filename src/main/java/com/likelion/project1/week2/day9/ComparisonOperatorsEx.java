ea041b5a0393e4df4d840e8c62367cec3b709219
hiyee-gj <gyoengjae.h.dev@gmail.com>
2023-04-27 10:43:03 +0900

    :sparkles: Add ComparisonOperatorsEx

---
+++ src/main/java/com/likelion/project1/week2/day9/ComparisonOperatorsEx.java
@@ -0,0 +1,14 @@
+package com.likelion.project1.week2.day9;
+
+public class ComparisonOperatorsEx {
+    public static void main(String[] args) {
+        int iVal1 = 10;
+        int iVal2 = 20;
+        System.out.printf("%d == %d --> %b\n", iVal1, iVal2, iVal1 == iVal2);
+        System.out.printf("%d != %d --> %b\n", iVal1, iVal2, iVal1 != iVal2);
+        System.out.printf("%d < %d --> %b\n", iVal1, iVal2, iVal1 < iVal2);
+        System.out.printf("%d <= %d --> %b\n", iVal1, iVal2, iVal1 <= iVal2);
+        System.out.printf("%d > %d --> %b\n", iVal1, iVal2, iVal1 > iVal2);
+        System.out.printf("%d >= %d --> %b\n", iVal1, iVal2, iVal1 >= iVal2);
+    }
+}
