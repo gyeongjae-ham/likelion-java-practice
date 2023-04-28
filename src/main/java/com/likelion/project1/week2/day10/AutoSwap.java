ec1d32f221ab8b7fc9425880d7a1f7b2de02db5f
hiyee-gj <gyoengjae.h.dev@gmail.com>
2023-04-28 11:05:09 +0900

    :sparkles: Add AutoSwap

---
+++ src/main/java/com/likelion/project1/week2/day10/AutoSwap.java
@@ -0,0 +1,17 @@
+package com.likelion.project1.week2.day10;
+
+import java.util.Arrays;
+
+public class AutoSwap {
+    public static void main(String[] args) {
+        int[] arr = {2, 1, 7, 9};
+        boolean check = arr[0] > arr[1];
+        if (check) {
+            int tmp = arr[0];
+            arr[0] = arr[1];
+            arr[1] = tmp;
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
