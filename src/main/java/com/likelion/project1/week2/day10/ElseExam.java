1aa567cf1d6c290a2c0607f76c49999752a5c6e9
hiyee-gj <gyoengjae.h.dev@gmail.com>
2023-04-28 09:20:44 +0900

    :sparkles: Add If 실습 내용

---
+++ src/main/java/com/likelion/project1/week2/day10/ElseExam.java
@@ -0,0 +1,19 @@
+package com.likelion.project1.week2.day10;
+
+public class ElseExam {
+    public static void main(String[] args) {
+        boolean checkTrue = true;
+        if (checkTrue) {
+            System.out.println("true 입니다");
+        } else {
+            System.out.println("false 입니다");
+        }
+
+        checkTrue = false;
+        if (checkTrue) {
+            System.out.println("true 입니다");
+        } else {
+            System.out.println("false 입니다");
+        }
+    }
+}
---
+++ src/main/java/com/likelion/project1/week2/day10/IfEx.java
@@ -0,0 +1,12 @@
+package com.likelion.project1.week2.day10;
+
+public class IfEx {
+    public static void main(String[] args) {
+        if (true) {
+            System.out.println("항상 실행 됩니다.");
+        }
+        if (false) {
+            System.out.println("항상 실행 됩니다.");
+        }
+    }
+}
