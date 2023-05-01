c07d0b18c70c945b19f3cab4c6a04f7240d5c55c
hiyee-gj <gyoengjae.h.dev@gmail.com>
2023-05-01 09:07:37 +0900

    :sparkles: Add CodeUp 1106

---
+++ src/main/java/com/likelion/project1/codeup/CodeUp1106.java
@@ -0,0 +1,10 @@
+package com.likelion.project1.codeup;
+
+public class CodeUp1106 {
+    public static void main(String[] args) {
+        int min = Integer.MIN_VALUE;
+        int max = Integer.MAX_VALUE;
+
+        System.out.println(min + " " + max);
+    }
+}
