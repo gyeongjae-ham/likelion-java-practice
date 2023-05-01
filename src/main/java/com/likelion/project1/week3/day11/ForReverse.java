f0ea0d0dc6a533e297f0dd1c7aae19575b45a05a
hiyee-gj <gyoengjae.h.dev@gmail.com>
2023-05-01 11:26:58 +0900

    :sparkles: Add ForReverse 예제

---
+++ src/main/java/com/likelion/project1/week3/day11/ForReverse.java
@@ -0,0 +1,10 @@
+package com.likelion.project1.week3.day11;
+
+public class ForReverse {
+    public static void main(String[] args) {
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i);
+            System.out.printf("%d ", i);
+        }
+    }
+}
---
+++ src/main/java/com/likelion/project1/week3/day11/ForReverseToZero.java
@@ -0,0 +1,9 @@
+package com.likelion.project1.week3.day11;
+
+public class ForReverseToZero {
+    public static void main(String[] args) {
+        for (int i = 10; i >= 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
